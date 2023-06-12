9f4c93102f7ebe5697d68e64ebf922adc1350545
RenoirLover <TeenPhilosopher@users.noreply.github.com>
2023-06-12 23:30:25 +0900

    Update musicgen.py

--- audiocraft/models/musicgen.py
+++ audiocraft/models/musicgen.py
@@ -291,14 +291,17 @@ class MusicGen:
             else:
                 # Calculate the start and end points for the melody slice
                 print("Melody sample rate is: + melody_sr)
-                start_frame = int((len(sections) - 1) * (window_len_secs-slide_secs) * melody_sr) + int(window_len_secs * melody_sr)
-                end_frame = start_frame + int((window_len_secs-slide_secs) * melody_sr)
+                current_time_in_secs = (len(sections) - 1) * (window_len_secs-slide_secs) + window_len_secs
+                start_frame = int(current_time_in_secs * melody_sr)
+                end_frame = start_frame + int(window_len_secs * melody_sr)
                 # Slice the melody tensor according to the current time position
                 print("Start frame: ", start_frame)
                 print("End frame: ", end_frame)
                 print("Melody shape: ", melody.shape)
                 melody_slice = melody[:, :, start_frame:end_frame]
                 print("Melody slice shape: ", melody_slice.shape)
+                start_frame = start_frame % melody.shape[2]
+                end_frame = end_frame % melody.shape[2]
                 section = self.generate_continuation_with_melody(prompt, sample_rate, melody_wavs=melody_slice, melody_sample_rate=melody_sr, descriptions=[description], progress=progress)
             print("Section shape before slicing: ", section.shape)
             section = section[:, :, int(slide_secs*sample_rate):]
