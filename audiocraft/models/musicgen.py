44402c4a6af8b2cb9358f053c31521b1fc9354ff
RenoirLover <TeenPhilosopher@users.noreply.github.com>
2023-06-12 23:48:36 +0900

    Update musicgen.py

--- audiocraft/models/musicgen.py
+++ audiocraft/models/musicgen.py
@@ -26,6 +26,15 @@ from ..utils.autocast import TorchAutocast
 MelodyList = tp.List[tp.Optional[torch.Tensor]]
 MelodyType = tp.Union[torch.Tensor, MelodyList]
 
+def wrap_around_slice(tensor, start, end):
+    if end <= tensor.shape[2]:
+        return tensor[:, :, start:end]
+    else:
+        part1 = tensor[:, :, start:]
+        part2 = tensor[:, :, :(end % tensor.shape[2])]
+        return torch.cat([part1, part2], dim=2)
+
+
 
 class MusicGen:
     """MusicGen main model with convenient generation API.
@@ -290,18 +299,12 @@ class MusicGen:
                 section = self.generate_continuation(prompt, sample_rate, descriptions=[description], progress=progress)
             else:
                 # Calculate the start and end points for the melody slice
-                print("Melody sample rate is: ", melody_sr)
                 current_time_in_secs = (len(sections) - 1) * (window_len_secs-slide_secs) + window_len_secs
                 start_frame = int(current_time_in_secs * melody_sr)
                 end_frame = start_frame + int(window_len_secs * melody_sr)
-                # Slice the melody tensor according to the current time position
-                print("Start frame: ", start_frame)
-                print("End frame: ", end_frame)
-                print("Melody shape: ", melody.shape)
-                melody_slice = melody[:, :, start_frame:end_frame]
-                print("Melody slice shape: ", melody_slice.shape)
-                #start_frame = start_frame % melody.shape[2]
-                #end_frame = end_frame % melody.shape[2]
+                # Slice the melody tensor according to the current time position and wrap around if its too long for the melody tensor
+                melody_slice = wrap_around_slice(melody, start_frame, end_frame)
+
                 section = self.generate_continuation_with_melody(prompt, sample_rate, melody_wavs=melody_slice, melody_sample_rate=melody_sr, descriptions=[description], progress=progress)
             print("Section shape before slicing: ", section.shape)
             section = section[:, :, int(slide_secs*sample_rate):]
